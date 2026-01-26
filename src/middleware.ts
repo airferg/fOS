5ffdea0808c2e2a56fa05d9855cc4b0f1e429b52
AIRFERG <wesmccoy23@gmail.com>
2026-01-26 09:42:21 -0500

    fix: exclude static files from middleware auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -104,7 +104,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - public files (images, svgs, etc.)
      */
-    '/((?!_next/static|_next/image|favicon.ico).*)',
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.jpg$|.*\\.jpeg$|.*\\.gif$|.*\\.svg$|.*\\.ico$|.*\\.webp$).*)',
   ],
 }
